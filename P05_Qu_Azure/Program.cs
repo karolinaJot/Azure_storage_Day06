7d2003b9fae3410414f61323e8810c4aaa8a8d34
karolinaJot <amicus.karo@gmail.com>
2023-05-15 22:51:21 +0200

    work with azure data lake

---
+++ P05_Qu_Azure/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+
+var connectionString = "DefaultEndpointsProtocol=https;AccountName=volleyballsa2;AccountKey=iy/BJyPxlSLlryA5mgKpwTYIP/HFfNW6KSZFUMIzY9vfWuY0ght3IDdrZ1anf9n0C4A1XuxLZ/rk+AStjvr8lw==;EndpointSuffix=core.windows.net";
+
+var queueName = "messages";
+
+QueueClient queueClient = new QueueClient(connectionString, queueName);
+
+await queueClient.CreateIfNotExistsAsync();
+
+Console.WriteLine("Enter a message to add to the queue:");
+var message = Console.ReadLine();
+await queueClient.SendMessageAsync(message);
+
+Console.WriteLine($"Message '{message}' added to the queue.");
+
+Console.WriteLine("Retrieving a message from the queue...");
+
+QueueMessage[] retrievedMessage = await queueClient.ReceiveMessagesAsync();
+if (retrievedMessage != null && retrievedMessage.Length > 0)
+{
+	Console.WriteLine($"Message retrieved: '{retrievedMessage[0].MessageText}'");
+	Console.WriteLine("Deleting the message from the queue...");
+	await queueClient.DeleteMessageAsync(retrievedMessage[0].MessageId,
+   retrievedMessage[0].PopReceipt);
+	Console.WriteLine("Message deleted successfully.");
+}
+else
+{
+	Console.WriteLine("No messages found in the queue.");
+}
---
+++ P06_DataLake_Azure/Program.cs
@@ -0,0 +1,44 @@
+﻿using Azure.Storage.Files.DataLake;
+using Microsoft.Extensions.Configuration;
+
+
+var configuration = new ConfigurationBuilder()
+	.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+	.Build();
+
+string connectionString = configuration.GetConnectionString("DataLakeConnectionString");
+
+
+//var connectionString = "DefaultEndpointsProtocol=https;AccountName=datalakestoragekj;AccountKey=6Un4cDKY02Wa4nhXKpwgIULyCGW2vwl9z52tEyOkLCut/r0OAK0aPUK/4ubJPbziReW+aLUK6Pdn+AStlXOGxw==;EndpointSuffix=core.windows.net";
+var containerName = "new-datalake-storage";
+
+//var connectionString = "myconnection";
+//var containerName = "mystoragecontainer";
+
+
+var dataLakeServiceClient = new DataLakeServiceClient(connectionString);
+var dataLakeContainerClient = dataLakeServiceClient.GetFileSystemClient(containerName);
+
+Console.WriteLine("Enter the file path of the image to upload:");
+var imagePath = Console.ReadLine();
+
+using var fileStream = File.OpenRead(imagePath);
+var blobName = Path.GetFileName(imagePath);
+
+var blobClient = dataLakeContainerClient.GetFileClient(blobName);
+await blobClient.UploadAsync(fileStream, true);
+Console.WriteLine($"Image '{imagePath}' uploaded successfully to Data Lake Storage Gen2.");
+
+Console.WriteLine("Enter the name of the image to download from Data Lake Storage Gen2:");
+var imageNameToDownload = Console.ReadLine();
+
+var downloadBlobClient = dataLakeContainerClient.GetFileClient(imageNameToDownload);
+Console.WriteLine("Enter the destination file path to save the downloaded image:");
+var destinationPath = Console.ReadLine();
+
+var download = await downloadBlobClient.ReadAsync();
+using var downloadFileStream = File.OpenWrite(destinationPath);
+
+await download.Value.Content.CopyToAsync(downloadFileStream);
+downloadFileStream.Close();
+Console.WriteLine($"Image '{imageNameToDownload}' downloaded successfully from Data Lake Storage Gen2 and saved to '{destinationPath}'.");
